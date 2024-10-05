287da3c64fea67938e5a93e888c89dd01bc31065
Rei1449 <kiriya4673@gmail.com>
2024-10-05 14:37:02 +0900

    aタグでエラーが出るかの確認

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,10 @@
 import HelloWorld from "@/components/HelloWorld";
 import TestClient from "@/components/TestClient";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
       {/* <Link href="/test">テスト</Link> */}
-      <Link href="/test.html">html</Link>
+      <a href="/test.html">html</a>
       <TestClient />
       <HelloWorld />
     </div>
