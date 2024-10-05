7023c82022a63ceff37ed07e5d18956ea3a2535d
Rei1449 <kiriya4673@gmail.com>
2024-10-05 14:30:23 +0900

    htmlの方をコメントアウトすると404になるため、もう片方の方をコメントアウトする

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="/test">テスト</Link>
-      {/* <Link href="/test.html">html</Link> */}
+      {/* <Link href="/test">テスト</Link> */}
+      <Link href="/test.html">html</Link>
       <TestClient />
       <HelloWorld />
     </div>
